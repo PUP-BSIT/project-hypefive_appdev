3eefa4fcd1a6d2be3155acf372d0abc4e57ce530
ternalapriljoy <ternalapriljoy@gmail.com>
2024-05-21 18:46:21 +0800

    add each sidebar function's components.

---
+++ orgbee/src/app/pages/events/events.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-events',
+  templateUrl: './events.component.html',
+  styleUrl: './events.component.css'
+})
+export class EventsComponent {
+
+}
---
+++ orgbee/src/app/pages/members/members.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MembersComponent } from './members.component';
+
+describe('MembersComponent', () => {
+  let component: MembersComponent;
+  let fixture: ComponentFixture<MembersComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [MembersComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(MembersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
