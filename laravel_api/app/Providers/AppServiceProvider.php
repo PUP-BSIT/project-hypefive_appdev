11f6900a64fb8a489f712e8809652c21fce718c9
Villa-VillaDiana <diana.villavilla02@gmail.com>
2024-05-24 17:18:16 +0800

    create tables

--- laravel_api/app/Providers/AppServiceProvider.php
+++ laravel_api/app/Providers/AppServiceProvider.php
@@ -3,22 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
 
-class AppServiceProvider extends ServiceProvider
-{
+class AppServiceProvider extends ServiceProvider {
     /**
      * Register any application services.
      */
-    public function register(): void
-    {
+    public function register(): void {
         //
     }
 
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
-    {
-        //
+    public function boot(): void {
+        Schema::defaultStringLength(191);
     }
 }
