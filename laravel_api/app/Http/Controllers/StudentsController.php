57248f9f8e6619886c50d85fb326abbd4db71959
Villa-VillaDiana <diana.villavilla02@gmail.com>
2024-05-24 21:23:36 +0800

    add route and update controller to add new students

--- laravel_api/app/Http/Controllers/StudentsController.php
+++ laravel_api/app/Http/Controllers/StudentsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Students;
+use App\Models\Login;
 use Illuminate\Support\Facades\DB;
 
 class StudentsController extends Controller
@@ -13,12 +14,32 @@ class StudentsController extends Controller
         return response()->json(Students::all(), 200);
     }
 
-    public function getStudentsById($id){
+    public function getStudentsById($id) {
         $student = Students::find($id);
 
-        if (is_null($student)){
+        if (is_null($student)) {
             return response()->json(['message'=>'Employee not Found'], 404);
         }
         return response()->json($student::find ($id), 200);
+    }
+
+    public function addStudent(Request $request) {
+        //Insert the data and pass the id
+        $student = DB::table('students')->insertGetId([
+            'first_name' => $request->first_name,
+            'last_name' => $request->last_name,
+            'email' => $request->email,
+            'birthday' => $request->birthday,
+            'gender' => $request->gender,
+        ]);
+
+        DB::table('login')->insert([
+            'user_id' => $student,
+            'student_number' => $request->student_number,
+            'password' => $request->password,
+        ]);
+
+        return response()->json(['Student added successfully'], 404);
+        
     }
 }
--- laravel_api/routes/api.php
+++ laravel_api/routes/api.php
@@ -8,6 +8,13 @@ Route::get('/user', function (Request $request) {
 })->middleware('auth:sanctum');
 
 //Get all students
-Route::get('/students', [\App\Http\Controllers\StudentsController::class, 'getStudents'])->name('api.students');
+Route::get('/students', [\App\Http\Controllers\StudentsController::class, 
+                             'getStudents'])->name('api.students');
 
-Route::get('/students/{id}', [\App\Http\Controllers\StudentsController::class, 'getStudentsById'])->name('api.students');
+//Get students by Id
+Route::get('/students/{id}', [\App\Http\Controllers\StudentsController::class, 
+                                'getStudentsById'])->name('api.students');
+
+//Add new student
+Route::post('/addStudent', [\App\Http\Controllers\StudentsController::class, 
+                                'addStudent'])->name('api.new-student');
