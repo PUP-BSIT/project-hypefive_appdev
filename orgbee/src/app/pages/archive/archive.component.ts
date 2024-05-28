0fa66df0db00af3f92671fc4627f06aa33c8b32e
almeramentok <almerageementok@gmail.com>
2024-05-28 10:16:36 +0800

    add archive-page files

--- orgbee/src/app/pages/archive/archive.component.ts
+++ orgbee/src/app/pages/archive/archive.component.ts
@@ -3,8 +3,30 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-archive',
   templateUrl: './archive.component.html',
-  styleUrl: './archive.component.css'
+  styleUrls: ['./archive.component.css']
 })
-export class ArchiveComponent {
 
+export class ArchiveComponent {
+  events = [
+    { icon: '../../../assets/icon.jpg'},
+    { icon: '../../../assets/logo.png'},
+    { icon: '../../../assets/icon.jpg'},
+    { icon: '../../../assets/icon.jpg'},
+    { icon: '../../../assets/icon.jpg'},
+    { icon: '../../../assets/logo.png'},
+  ];
+  currentSlide = 0;
+
+  moveLeft() {
+    if (this.currentSlide > 0) {
+      this.currentSlide -= 3;
+    }
+  }
+
+  moveRight() {
+    const maxSlides = Math.ceil(this.events.length / 3) - 1;
+    if (this.currentSlide < maxSlides * 3) {
+      this.currentSlide += 3;
+    }
+  }
 }
