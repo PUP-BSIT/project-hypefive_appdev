8afd1dd503df843841ae568e82247a5835917e07
almeramentok <almerageementok@gmail.com>
2024-05-29 20:16:38 +0800

    edit all exceeded 80 cols

--- orgbee/src/app/pages/members/members.component.ts
+++ orgbee/src/app/pages/members/members.component.ts
@@ -15,15 +15,20 @@ export class MembersComponent implements OnInit {
     const members = document.querySelectorAll('.member');
     const modal = document.getElementById('memberModal') as HTMLElement;
     const span = document.getElementsByClassName('close')[0] as HTMLElement;
-    const removeButton = modal.querySelector('button.remove') as HTMLButtonElement;
+    const removeButton = 
+      modal.querySelector('button.remove') as HTMLButtonElement;
 
     members.forEach(member => {
       member.addEventListener('click', (event) => {
-        const memberName = (member.querySelector('#name') as HTMLElement).innerText;
-        const memberIconSrc = (member.querySelector('img') as HTMLImageElement).src;
+        const memberName = 
+          (member.querySelector('#name') as HTMLElement).innerText;
+        const memberIconSrc = 
+          (member.querySelector('img') as HTMLImageElement).src;
 
-        (document.getElementById('modal-name') as HTMLElement).innerText = memberName;
-        (document.getElementById('modal-icon') as HTMLImageElement).src = memberIconSrc;
+        (document.getElementById('modal-name') as HTMLElement).innerText = 
+          memberName;
+        (document.getElementById('modal-icon') as HTMLImageElement).src = 
+          memberIconSrc;
 
         modal.style.display = 'block';
       });
@@ -34,7 +39,8 @@ export class MembersComponent implements OnInit {
     };
 
     removeButton.addEventListener('click', () => {
-      const memberName = (document.getElementById('modal-name') as HTMLElement).innerText;
+      const memberName = 
+        (document.getElementById('modal-name') as HTMLElement).innerText;
       console.log(`Remove member: ${memberName}`);
       modal.style.display = 'none';
     });
