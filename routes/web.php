13385a9247f9d9e91fa663f1c4b0268f22f68cde
Villa-VillaDiana <diana.villavilla02@gmail.com>
2024-05-31 17:42:02 +0800

    update routing

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\CheckToken;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/login', function () {
+    return view('welcome');
+});
+
+Route::middleware([CheckToken::class])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('welcome');
+    });
+
+    Route::get('/freedom-wall', function () {
+        return view('welcome');
+    });
+
+    Route::get('/events', function () {
+        return view('welcome');
+    });
+
+    Route::get('/members', function () {
+        return view('welcome');
+    });
+
+    Route::get('/archive', function () {
+        return view('welcome');
+    });
 });
