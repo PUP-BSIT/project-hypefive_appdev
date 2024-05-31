fbc22a3ca02c90b5cc8b507a554bd8d490161e0e
Villa-VillaDiana <diana.villavilla02@gmail.com>
2024-05-31 17:24:28 +0800

    update web routing

--- laravel_api/routes/web.php
+++ laravel_api/routes/web.php
@@ -5,3 +5,27 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('welcome');
+});
+
+Route::get('/freedom-wall', function () {
+    return view('welcome');
+});
+
+Route::get('/events', function () {
+    return view('welcome');
+});
+
+Route::get('/members', function () {
+    return view('welcome');
+});
+
+Route::get('/archive', function () {
+    return view('welcome');
+});
