1f288eaf7aef3b0d63ace9321e18490d95554e3e
ternalapriljoy <ternalapriljoy@gmail.com>
2024-06-01 03:08:48 +0800

    create api endpoints for announcement.

---
+++ laravel_api/app/Http/Controllers/Announce.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Announcement;
+use Illuminate\Http\Request;
+
+class Announce extends Controller
+{
+    public function getAnnouncements()
+    {
+        return Announcement::where('is_posted', 1)->get();
+    }
+
+    public function createAnnouncement(Request $request)
+    {
+        $validatedData = $request->validate([
+            'subject' => 'required|max:350',
+            'recipient' => 'required|integer',
+            'content' => 'required',
+            'is_posted' => 'boolean',
+            'student_id' => 'required|exists:students,user_id',
+        ]);
+    
+        $announcement = Announcement::create($validatedData);
+        $announcement->update(['is_posted' => 1]);
+    
+        $announcementId = $announcement->id;
+        return response()->json([
+            'announcement' => $announcement,
+            'announcement_id' => $announcementId 
+        ], 201);
+    }
+
+    public function show($id)
+    {
+        return Announcement::findOrFail($id);
+    }
+
+    public function updateAnnouncement(Request $request, $id)
+    {
+        $validatedData = $request->validate([
+            'subject' => 'required|max:350',
+            'recipient' => 'required|integer',
+            'content' => 'required',
+            'is_posted' => 'boolean',
+            'student_id' => 'required|exists:students,user_id',
+        ]);
+
+        $announcement = Announcement::findOrFail($id);
+        $announcement->update($validatedData);
+
+        return response()->json($announcement, 200);
+    }
+
+    public function deleteAnnouncement($id)
+    {
+        $announcement = Announcement::findOrFail($id);
+        $announcement->update(['is_posted' => 0]);
+    
+        return response()->json(null, 204);
+    }
+    
+}
