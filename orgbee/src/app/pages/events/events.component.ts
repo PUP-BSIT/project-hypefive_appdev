f7e1ed292e70d24ff8701dcd8935bcacd6eb7d99
cainday_bradley <bradleycainday@gmail.com>
2024-06-05 19:19:56 +0800

    add manage button functionality

--- orgbee/src/app/pages/events/events.component.ts
+++ orgbee/src/app/pages/events/events.component.ts
@@ -12,10 +12,14 @@ export class EventsComponent implements OnInit {
   activeTab = 'UPCOMING'; // Tracks the active tab
   events: any[] = []; // Array to store upcoming events
   drafts: any[] = []; // Array to store draft events
+  occurringEvents: any[] = []; // Array to store occurring events
   filteredEvents: any[] = []; // Array to store events based on active tab
   isModalVisible = false; // To control visibility of modal
+  isManageModalVisible = false; // To control visibility of manage modal
+  selectedEvent: any; // The event currently being managed
+  isEditMode = false; // Flag to track if we are in edit mode
+  editingEventIndex = -1; // Index of the event being edited
 
-  // Initialize the form group with form controls and validators
   constructor(private fb: FormBuilder) {
     this.eventForm = this.fb.group({
       eventName: ['', Validators.required],
@@ -32,10 +36,7 @@ export class EventsComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    // Display events based on the active tab 
     this.displayEvents(this.activeTab);
-
-    // Enable/disable if nag yes si user
     this.eventForm.get('regFee')?.valueChanges.subscribe((value) => {
       const regAmountControl = this.eventForm.get('regAmount');
       if (value === 'yes') {
@@ -47,73 +48,123 @@ export class EventsComponent implements OnInit {
   }
 
   openModal(): void {
-    // Open the modal
     this.isModalVisible = true;
   }
 
   closeModal(): void {
-    // Close the modal
     this.isModalVisible = false;
+    this.isEditMode = false;
+    this.editingEventIndex = -1;
+  }
+
+  openManageModal(event: any): void {
+    this.selectedEvent = event;
+    this.isManageModalVisible = true;
+  }
+
+  closeManageModal(): void {
+    this.isManageModalVisible = false;
+  }
+
+  editEvent(event: any): void {
+    this.closeManageModal();
+    this.openModal();
+    this.eventForm.patchValue(event);
+    this.isEditMode = true;
+    this.editingEventIndex = this.filteredEvents.indexOf(event);
+  }
+
+  markAsOccurring(event: any): void {
+    if (this.activeTab === 'UPCOMING') {
+      const index = this.events.indexOf(event);
+      if (index > -1) {
+        this.events.splice(index, 1);
+        this.occurringEvents.push(event);
+      }
+    } else if (this.activeTab === 'DRAFTS') {
+      const index = this.drafts.indexOf(event);
+      if (index > -1) {
+        this.drafts.splice(index, 1);
+        this.occurringEvents.push(event);
+      }
+    }
+    this.closeManageModal();
+    this.displayEvents(this.activeTab);
+  }
+
+  cancelEvent(event: any): void {
+    if (this.activeTab === 'UPCOMING') {
+      const index = this.events.indexOf(event);
+      if (index > -1) {
+        this.events.splice(index, 1);
+      }
+    } else if (this.activeTab === 'DRAFTS') {
+      const index = this.drafts.indexOf(event);
+      if (index > -1) {
+        this.drafts.splice(index, 1);
+      }
+    } else if (this.activeTab === 'RECURRING') {
+      const index = this.occurringEvents.indexOf(event);
+      if (index > -1) {
+        this.occurringEvents.splice(index, 1);
+      }
+    }
+    this.closeManageModal();
+    this.displayEvents(this.activeTab);
   }
 
   displayEvents(tab: string): void {
-    // Update active tab and filter events accordingly
     this.activeTab = tab;
-
     if (tab === 'UPCOMING') {
-      this.filteredEvents = this.events; // Display upcoming events
+      this.filteredEvents = this.events;
     } else if (tab === 'DRAFTS') {
-      this.filteredEvents = this.drafts; // Display draft events
+      this.filteredEvents = this.drafts;
+    } else if (tab === 'RECURRING') {
+      this.filteredEvents = this.occurringEvents;
     }
   }
 
-  get formControls() {
-    // Getter for form controls to simplify template code
-    return this.eventForm.controls;
-  }
-
   nextStep(): void {
-    // Move to the next step in a multi-step form
     if (this.currentStep < 3) {
       this.currentStep++;
     }
   }
 
   prevStep(): void {
-    // Move to the previous step in a multi-step form
     if (this.currentStep > 0) {
       this.currentStep--;
     }
   }
 
   submitForm(type: string): void {
-    // Submit the form and add the event to either events or drafts 
     if (this.eventForm.valid) {
-      const newEvent = { 
-        eventName: this.eventForm.value.eventName,
-        eventLocation: this.eventForm.value.eventLocation,
-        eventDate: this.eventForm.value.eventDate,
-        eventTime: this.eventForm.value.eventTime,
-        attendance: this.eventForm.value.attendance,
-        regFee: this.eventForm.value.regFee,
-        regAmount: this.eventForm.value.regAmount,
-        maxAttendees: this.eventForm.value.maxAttendees,
-        eventCaption: this.eventForm.value.eventCaption,
-        eventPoster: this.eventForm.value.eventPoster,
-      };
-
-      if (type === 'publish') { 
-        this.events.push(newEvent);
-      } else { 
-        this.drafts.push(newEvent);
+      const newEvent = this.eventForm.value;
+      if (this.isEditMode) {
+        if (this.activeTab === 'UPCOMING') {
+          if (type === 'publish') {
+            this.events[this.editingEventIndex] = newEvent;
+          } else {
+            this.drafts[this.editingEventIndex] = newEvent;
+          }
+        } else if (this.activeTab === 'DRAFTS') {
+          this.drafts[this.editingEventIndex] = newEvent;
+        } else if (this.activeTab === 'RECURRING') {
+          this.occurringEvents[this.editingEventIndex] = newEvent;
+        }
+        this.isEditMode = false;
+        this.editingEventIndex = -1;
+      } else {
+        if (type === 'publish') {
+          this.events.push(newEvent);
+        } else {
+          this.drafts.push(newEvent);
+        }
       }
-
       this.closeModal();
       this.currentStep = 0;
       this.eventForm.reset();
       this.displayEvents(this.activeTab);
     } else {
-      // alert user to fill in all required fields
       alert('Please fill in all required fields.');
     }
   }
