43647a95fa0580866dedc5ddc5e0ea3ee0a58d33
Villa-VillaDiana <diana.villavilla02@gmail.com>
2024-06-05 15:09:32 +0800

    add new tables and create adminseeder

--- laravel_api/database/migrations/0001_01_01_000002_create_cache_table.php
+++ laravel_api/database/migrations/0001_01_01_000002_create_cache_table.php
@@ -11,17 +11,17 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('cache', function (Blueprint $table) {
-            $table->string('key')->primary();
-            $table->mediumText('value');
-            $table->integer('expiration');
-        });
+        // Schema::create('cache', function (Blueprint $table) {
+        //     $table->string('key')->primary();
+        //     $table->mediumText('value');
+        //     $table->integer('expiration');
+        // });
 
-        Schema::create('cache_locks', function (Blueprint $table) {
-            $table->string('key')->primary();
-            $table->string('owner');
-            $table->integer('expiration');
-        });
+        // Schema::create('cache_locks', function (Blueprint $table) {
+        //     $table->string('key')->primary();
+        //     $table->string('owner');
+        //     $table->integer('expiration');
+        // });
     }
 
     /**
@@ -29,7 +29,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('cache');
-        Schema::dropIfExists('cache_locks');
+        // Schema::dropIfExists('cache');
+        // Schema::dropIfExists('cache_locks');
     }
 };
--- laravel_api/database/seeders/AccountStatusSeeder.php
+++ laravel_api/database/seeders/AccountStatusSeeder.php
@@ -13,7 +13,7 @@ class AccountStatusSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('account_statuses')-> insert([
+        DB::table('account_status')-> insert([
             ['status'=>'pending'],
             ['status'=>'accepted'],
             ['status'=>'declined']
