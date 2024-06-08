ea207e38bd065ae47ae0f9d250632e38f44f4cda
ternalapriljoy <ternalapriljoy@gmail.com>
2024-06-08 08:38:21 +0800

    remove event emitter.

--- orgbee/src/app/pages/dashboard/an-modal/an-modal.component.ts
+++ orgbee/src/app/pages/dashboard/an-modal/an-modal.component.ts
@@ -1,20 +1,7 @@
-import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
-import {
-  FormBuilder,
-  FormGroup,
-  Validators,
-  AbstractControl,
-  ValidatorFn,
-} from '@angular/forms';
-import { LoginService } from '../../../../service/login.service';
-import { AnnouncementService } from '../../../../service/announcement.service';
-
-export interface Announcement {
-  id: number; 
-  subject: string;
-  content: string;
-  recipient: string;
-}
+import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl } from '@angular/forms';
+import { LoginService, UserInfo } from '../../../../service/login.service';
+import { AnnouncementService, Announcement } from '../../../../service/announcement.service';
 
 @Component({
   selector: 'app-an-modal',
@@ -23,13 +10,12 @@ export interface Announcement {
 })
 
 export class AnModalComponent implements OnInit {
+  userInfo: UserInfo | null = null;
   announcements: Announcement[] = [];
-  userInfo: any = {};
 
-  @Input() showModal: boolean = false;
-  @Output() closeModal: EventEmitter<void> = new EventEmitter<void>();
-  @Output() announcementCreated: EventEmitter<Announcement> = new EventEmitter<Announcement>();
+  @Input() showModal = false;
   @Input() announcementForm: FormGroup;
+  @Input() handleAnnouncementCreated: (newAnnouncement: Announcement) => void;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -51,10 +37,6 @@ export class AnModalComponent implements OnInit {
     }
   }
 
-  close(): void {
-    this.closeModal.emit();
-  }
-
   ngOnInit(): void {
     this.announcementForm = this.formBuilder.group({
       subject: ['', [Validators.required]],
@@ -62,61 +44,60 @@ export class AnModalComponent implements OnInit {
       recipient: ['', Validators.required],
     });
 
-    this.loginService.onDataRetrieved((data: any) => {
+    this.loginService.onDataRetrieved((data: UserInfo) => {
       this.userInfo = data;
     });
   }
 
-  get subjectControl(): AbstractControl {
-    return this.announcementForm.get('subject')!;
+  get subjectControl(): AbstractControl | null {
+    return this.announcementForm.get('subject');
   }
 
-  get messageControl(): AbstractControl {
-    return this.announcementForm.get('message')!;
+  get messageControl(): AbstractControl | null {
+    return this.announcementForm.get('message');
   }
 
-  get recipientControl(): AbstractControl {
-    return this.announcementForm.get('recipient')!;
+  get recipientControl(): AbstractControl | null {
+    return this.announcementForm.get('recipient');
   }
 
   submitAnnouncement(): void {
-    if (this.announcementForm.valid) {
-      const token = localStorage.getItem('token');
-      if (token) {
-        const currentUserId = this.userInfo.user_id;
-        if (currentUserId) {
-          const newAnnouncement = {
-            subject: this.announcementForm.get('subject')?.value,
-            content: this.announcementForm.get('message')?.value,
-            recipient: this.announcementForm.get('recipient')?.value,
-            student_id: currentUserId,
-            id: 0
-          };
-
-          this.announcementService.createAnnouncement(newAnnouncement).subscribe(
-           (announcementId: number) => { 
+    if (this.announcementForm.valid && this.userInfo) {
+      const currentUserId = this.userInfo.user_id;
+      if (currentUserId) {
+        const newAnnouncement: Announcement = {
+          id: 0,
+          subject: this.subjectControl?.value,
+          content: this.messageControl?.value,
+          recipient: this.recipientControl?.value,
+          student_id: currentUserId
+        };
+  
+        this.announcementService.createAnnouncement(newAnnouncement).subscribe(
+          (announcementId: number) => {
             newAnnouncement.id = announcementId;
-              this.announcementForm.reset();
-              this.close();
-              this.announcementCreated.emit(newAnnouncement);
-              
-              alert('Announcement created successfully! ID: ' + announcementId);
-            },
-            (error) => {
-              console.error('Error creating announcement:', error);
-              alert('Error creating announcement. Please try again later.');
-            }
-          );
-        } else {
-          console.error('Error extracting user ID from token.');
-          alert('Error creating announcement. Please try again later.');
-        }
+            this.announcements.push(newAnnouncement); 
+            this.announcementForm.reset();
+            this.showModal = false; 
+            alert('Announcement created successfully! ID: ' + announcementId);
+            //TO-DO: handle announcement creation in ui
+          },
+          (error) => {
+            console.error('Error creating announcement:', error);
+            alert('Error creating announcement. Please try again later.');
+          }
+        );
       } else {
-        console.error('JWT token not found.');
+        console.error('Error extracting user ID from token.');
         alert('Error creating announcement. Please try again later.');
       }
     } else {
       this.announcementForm.markAllAsTouched();
     }
+  }
+  
+
+  closeModal(): void {
+    this.showModal = false;
   }
 }
