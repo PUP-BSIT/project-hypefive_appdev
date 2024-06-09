72b214993037b00a4851eb0f0540229dce093a4c
Villa-VillaDiana <diana.villavilla02@gmail.com>
2024-06-09 11:58:11 +0800

    create FreedomWallController and update api.php

--- laravel_api/routes/api.php
+++ laravel_api/routes/api.php
@@ -14,3 +14,11 @@ Route::post('/register', [\App\Http\Controllers\StudentsController::class,
 //Login
 Route::post('/login', [\App\Http\Controllers\StudentsController::class, 
                                 'login'])->name('api.login');
+
+Route::get('/getPosts', [\App\Http\Controllers\FreedomWallController::class, 
+                                'getPosts'])->name('api.getPosts');     
+
+Route::post('/createPostFW', 
+        [\App\Http\Controllers\FreedomWallController::class, 'createPostFW'])
+            ->name('api.createPostFW');
+
