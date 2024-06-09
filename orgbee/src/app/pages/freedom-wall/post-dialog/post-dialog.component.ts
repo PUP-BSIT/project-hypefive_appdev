a1e54652233167bdc7654b14c515948a19705d65
Yves John B. Tapispisan <bancolitayves@gmail.com>
2024-06-09 16:59:29 +0800

    fix use of vanilla js

--- orgbee/src/app/pages/freedom-wall/post-dialog/post-dialog.component.ts
+++ orgbee/src/app/pages/freedom-wall/post-dialog/post-dialog.component.ts
@@ -7,36 +7,13 @@ import { Post } from '../freedom-wall.component';
   templateUrl: './post-dialog.component.html',
   styleUrls: ['./post-dialog.component.css']
 })
-export class PostDialogComponent implements AfterViewInit {
+export class PostDialogComponent  {
   @Input() postSelected: Post;
 
   constructor(
     public dialogRef: MatDialogRef<PostDialogComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: Post,
-    private elementRef: ElementRef
-  ) {
+    @Inject(MAT_DIALOG_DATA) public data: Post) {
     this.postSelected = data;
   }
 
-  ngAfterViewInit() {
-    const dialogContent = this.elementRef.nativeElement.querySelector('.dialog-content');
-    if (dialogContent) {
-      this.adjustDialogHeight(dialogContent);
-    }
-  }
-// TODO: Tapispisan: Don't use vanilla js here.
-  adjustDialogHeight(dialogContent: HTMLElement) {
-    const titleHeight = dialogContent.querySelector('h4').scrollHeight;
-    const textHeight = dialogContent.querySelector('p').scrollHeight;
-    const totalHeight = titleHeight + textHeight + 30; 
-    const maxHeight = window.innerHeight - 100;
-
-    dialogContent.style.height = Math.min(totalHeight, maxHeight) + 'px';
-
-    if (totalHeight > maxHeight) {
-      dialogContent.style.overflowY = 'auto';
-    } else {
-      dialogContent.style.overflowY = 'visible';
-    }
-  }
 }
