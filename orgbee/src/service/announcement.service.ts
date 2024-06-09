e903bf6493c9ae1e46e3c207248e9f1153795d93
ternalapriljoy <ternalapriljoy@gmail.com>
2024-06-09 21:46:10 +0800

    format date using date pipe.

--- orgbee/src/service/announcement.service.ts
+++ orgbee/src/service/announcement.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { DatePipe } from '@angular/common';
 
 export interface Announcement {
   id: number;
@@ -23,13 +24,27 @@ export interface AnnouncementDisplay {
 @Injectable({
   providedIn: 'root'
 })
+
 export class AnnouncementService {
   private apiUrl = 'http://127.0.0.1:8000/api/announcements';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private datePipe: DatePipe) {}
 
   getAnnouncements(): Observable<AnnouncementDisplay[]> {
-    return this.http.get<AnnouncementDisplay[]>(this.apiUrl);
+    return this.http.get<AnnouncementDisplay[]>(this.apiUrl).pipe(
+      map((announcements: AnnouncementDisplay[]) => {
+        return announcements.map(announcement => {
+          return {
+            ...announcement,
+            created_at: this.formatDate(announcement.created_at)
+          };
+        });
+      })
+    );
+  }
+
+  private formatDate(dateString: string): string {
+    return this.datePipe.transform(dateString, 'MMMM d, yyyy');
   }
 
   createAnnouncement(announcement: Announcement): Observable<number> {
