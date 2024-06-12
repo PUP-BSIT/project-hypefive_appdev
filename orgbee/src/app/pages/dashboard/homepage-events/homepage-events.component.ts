31daae20a8cc8b85e8fd15aef8fc6a630ac695b3
ternalapriljoy <ternalapriljoy@gmail.com>
2024-06-12 18:43:49 +0800

    add modal animation.

--- orgbee/src/app/pages/dashboard/homepage-events/homepage-events.component.ts
+++ orgbee/src/app/pages/dashboard/homepage-events/homepage-events.component.ts
@@ -1,9 +1,23 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-homepage-events',
   templateUrl: './homepage-events.component.html',
-  styleUrls: ['./homepage-events.component.css']
+  styleUrls: ['./homepage-events.component.css'],
+  animations: [
+    trigger('modalAnimation', [
+      state('open', style({
+        opacity: 1,
+        transform: 'scale(1)'
+      })),
+      state('closed', style({
+        opacity: 0,
+        transform: 'scale(0.8)'
+      })),
+      transition('open <=> closed', animate('200ms ease-in-out'))
+    ])
+  ]
 })
 export class HomepageEventsComponent {
   isEventModalVisible = false;
