45d4b62c9b1631d17461890f74804b6132b8ca77
Villa-VillaDiana <diana.villavilla02@gmail.com>
2024-06-19 19:18:59 +0800

    deploy files from development branch

---
+++ app/Http/Controllers/EventsController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class EventsController extends Controller {
+
+  public function createEvent(Request $request) {
+    $event = $request->only(
+      'event_name', 'location', 'date', 'time', 'all_members_required',
+      'has_reg_fee', 'registration_fee', 'max_attendees', 'caption',
+      'poster_loc', 'event_status_id');
+    $event['created_at'] = now();
+
+    if ($event) {
+      DB::table('events')->insert($event);
+      $response['message'] = 'Event submitted successfully';
+      $response['code'] = 200;
+      return response()->json($response);
+    } else {
+      $response['message'] = 'Failed';
+      $response['code'] = 404;
+      return response()->json($response);
+    }
+  }
+
+  public function getUpcomingEvents() {
+    $upcoming = DB::table('events')->where('event_status_id', '=', 2)
+      ->where('event_state_id', '=', 1)
+      ->whereNotIn('event_state_id', [4])->orderBy('date', 'desc')->get();
+    return response()->json($upcoming, 200);
+  }
+
+  public function getDraftEvents() {
+    $upcoming = DB::table('events')->where('event_status_id', '=', 1)
+      ->whereNotIn('event_state_id', [4])->orderBy('date', 'desc')->get();
+    return response()->json($upcoming, 200);
+  }
+
+  public function getOccuringEvents() {
+    $upcoming = DB::table('events')->where('event_status_id', '=', 2)
+      ->where('event_state_id', '=', 2)
+      ->whereNotIn('event_state_id', [4])->orderBy('date', 'desc')->get();
+    return response()->json($upcoming, 200);
+  }
+
+  public function markAsOccuring(Request $request) {
+    $updateState = $request->only('id');
+
+    if ($updateState) {
+      DB::table('events')->where('id', $updateState)
+        ->update(['event_state_id' => 2, 'updated_at' => now()]);
+      $response['message'] = 'Event state updated successfully';
+      $response['code'] = 200;
+      return response()->json($response);
+    } else {
+      $response['message'] = 'Failed to update';
+      $response['code'] = 400;
+      return response()->json($response);
+    }
+  }
+
+  public function markAsComplete(Request $request) {
+    $updateState = $request->only('id');
+
+    if ($updateState) {
+      DB::table('events')->where('id', $updateState)
+        ->update(['event_state_id' => 3, 'updated_at' => now()]);
+      $response['message'] = 'Event state updated successfully';
+      $response['code'] = 200;
+      return response()->json($response);
+    } else {
+      $response['message'] = 'Failed to update';
+      $response['code'] = 400;
+      return response()->json($response);
+    }
+  }
+
+  public function publishDraft(Request $request) {
+    $updateState = $request->only('id');
+
+    if ($updateState) {
+      DB::table('events')->where('id', $updateState)
+        ->update([
+          'event_state_id' => 1,
+          'event_status_id' => 2,
+          'updated_at' => now()
+        ]);
+      $response['message'] = 'Event state updated successfully';
+      $response['code'] = 200;
+      return response()->json($response);
+    } else {
+      $response['message'] = 'Failed to update';
+      $response['code'] = 400;
+      return response()->json($response);
+    }
+  }
+
+  public function cancelEvent(Request $request) {
+    $cancelEvent = $request->only('id');
+
+    if ($cancelEvent) {
+      DB::table('events')->where('id', $cancelEvent)
+        ->update(['event_state_id' => 4, 'updated_at' => now()]);
+      $response['message'] = 'Event canceled updated successfully';
+      $response['code'] = 200;
+      return response()->json($response);
+    } else {
+      $response['message'] = 'Failed to cancel';
+      $response['code'] = 400;
+      return response()->json($response);
+    }
+  }
+
+  public function updateEvent(Request $request) {
+    $updateDetails = $request->only(
+      'event_name', 'location', 'date', 'time', 'all_members_required',
+      'has_reg_fee', 'registration_fee', 'max_attendees', 'caption',
+      'poster_loc', 'event_status_id');
+      
+    $updateDetails['updated_at'] = now();
+    $updateId = $request->id;
+
+    if ($updateDetails) {
+      DB::table('events')->where('id', $updateId)->update($updateDetails);
+      $response['message'] = 'Event updated successfully';
+      $response['code'] = 200;
+      return response()->json($response);
+    } else {
+      $response['message'] = 'Failed to update';
+      $response['code'] = 400;
+      return response()->json($response);
+    }
+  }
+}
--- app/Http/Controllers/StudentsController.php
+++ app/Http/Controllers/StudentsController.php
@@ -17,131 +17,175 @@ use App\Mail\VerificationMail;
 use App\Models\User;
 
 class StudentsController extends Controller {
-    public function register(Request $request) {
-        $student= User::where ('email', $request['email'])->first();
-        $student_number= Students::where('student_number', $request['student_number'])->first();
-         
-        if ($student ) {
-            $response['status'] = 0;
-            $response['message'] = 'Email already exists';
-            $response['code'] = 409;
-        } else if($student_number) {
-            $response['status'] = 0;
-            $response['message'] = 'Student number already exists';
-            $response['code'] = 409;
-        } else {
-            $verificationCode = Str::random(15);
-            //Insert the data 
-            $student = DB::table('users')->insertGetId([
-                'email' => $request->email,
-                'password' => bcrypt($request->password),
-                'email_auth_token' => $verificationCode,
-            ]);
-            DB::table('students')->insert([
-                'first_name' => $request->first_name,
-                'last_name' => $request->last_name,
-                'student_number' => $request->student_number,
-                'birthday' => $request->birthday,
-                'gender' => $request->gender,
-                'user_id' => $student,
-            ]);
-            $student = User::find($student);
-            $this->sendVerificationEmail($student);
-            
-            $response['status'] = 1;
-            $response['message'] = 
-                'User registered successfully, please wait for the approval';
-            $response['code'] = 200;
-        }
-        return response()->json($response);
+  public function register(Request $request) {
+    $student = User::where('email', $request['email'])->first();
+
+    $student_number = Students::where('student_number', 
+      $request['student_number'])->first();
+
+    $iconId = null;
+    $createTime = now();
+    if ($request->gender === 'female') {
+      $iconId = rand(1, 6); // Random picker from 1 to 6 for females
+    } elseif ($request->gender === 'male') {
+      $iconId = rand(7, 10); // Random picker from 7 to 10 for males
+    } else {
+      $iconId = rand(1, 10); // Random picker from 1 to 10 for others
     }
 
-    public function login(Request $request) {
-        $credentials = $request->only('email', 'password');
+    if ($student) {
+      $response['status'] = 0;
+      $response['message'] = 'Email already exists';
+      $response['code'] = 409;
+    } else if ($student_number) {
+      $response['status'] = 0;
+      $response['message'] = 'Student number already exists';
+      $response['code'] = 409;
+    } else {
+      $verificationCode = Str::random(15);
+      //Insert the data 
+      $student = DB::table('users')->insertGetId([
+        'email' => $request->email,
+        'password' => bcrypt($request->password),
+        'email_auth_token' => $verificationCode,
+        'created_at' => $createTime
+      ]);
+      DB::table('students')->insert([
+        'first_name' => $request->first_name,
+        'last_name' => $request->last_name,
+        'student_number' => $request->student_number,
+        'birthday' => $request->birthday,
+        'gender' => $request->gender,
+        'user_id' => $student,
+        'icon_id' => $iconId,
+        'created_at' => $createTime
+      ]);
+      $student = User::find($student);
+      $this->sendVerificationEmail($student);
 
-        try {
-            if (!JWTAuth::attempt($credentials)) {
-                $response['status'] = 0;
-                $response['code'] = 401;
-                $response['data'] = null;
-                $response['message'] = 'Email or password is incorrect';
+      $response['status'] = 1;
+      $response['message'] =
+        'User registered successfully, please wait for the approval';
+      $response['code'] = 200;
+    }
+    return response()->json($response);
+  }
 
-                return response()->json($response);
-            }
-        } catch(JWTException $e){
-            $response['data'] = null;
-            $response['code'] = 500;
-            $response['message'] = 'Could not create token';
+  public function login(Request $request) {
+    $credentials = $request->only('email', 'password');
 
-            return response()->json($response);
-        }
-        
-        $user = auth()->user();
-
-        // Check the account status of the user
-        if ($user->account_status_id != 2) {
-            $response['status'] = 0;
-            $response['code'] = 401;
-            $response['message'] = 'Please wait for the confirmation';
-            return response()->json($response);
-        }
-
-        $data['token'] = auth()->claims([
-            'user_id'=>$user->id,
-            'email' =>$user->email
-        ])->attempt($credentials);
-
-        $response['data'] = $data;
-        $response['status'] = 1;
-        $response['code'] = 200;
-        $response['message'] = 'Login successful';
+    try {
+      if (!JWTAuth::attempt($credentials)) {
+        $response['status'] = 0;
+        $response['code'] = 401;
+        $response['data'] = null;
+        $response['message'] = 'Email or password is incorrect';
 
         return response()->json($response);
+      }
+    } catch (JWTException $e) {
+      $response['data'] = null;
+      $response['code'] = 500;
+      $response['message'] = 'Could not create token';
+
+      return response()->json($response);
     }
 
-    public function sendVerificationEmail($student) {
-        $verificationCode = $student->email_auth_token;
-        
-        Mail::to($student->email)->send(new VerificationMail($verificationCode));
-    }
-    
-    public function retrieve(Request $request, $id, $email) {
-        try {
-            // Verify JWT token
-            $user = JWTAuth::parseToken()->authenticate();
-        } catch (TokenExpiredException $e) {
-            return response()->json(['message' => 'Token expired'], 401);
-        } catch (TokenInvalidException $e) {
-            return response()->json(['message' => 'Token invalid'], 401);
-        } catch (JWTException $e) {
-            return response()->json(['message' => 'Token absent'], 401);
-        }
+    $user = auth()->user();
 
-        // Check if the user's ID and email match the parameters
-        if ($user->id == $id && $user->email == $email) {
-            // Retrieve student and user information based on user_id
-            $student = Students::where('user_id', $id)->first();
-            $userInfo = User::where('email', $email)->first();
-
-            if ($student && $userInfo) {
-                return response()->json([
-                    'first_name' => $student->first_name,
-                    'last_name' => $student->last_name,
-                    'email' => $userInfo->email,
-                    'student_number' => $student->student_number,
-                    'birthday' => $student->birthday,
-                    'gender' => $student->gender,
-                    'user_id' => $student->user_id,
-                    'role_id' => $student->role_id, 
-                    'account_status_id' => $student->account_status_id, 
-                ]);
-            } else {
-                // Student or user not found
-                return response()->json(['message' => 'Student or user not found'], 404);
-            }
-        } else {
-            // Unauthorized access
-            return response()->json(['message' => 'Unauthorized'], 403);
-        }
+    // Check the account status of the user
+    if ($user->account_status_id != 2) {
+      $response['status'] = 0;
+      $response['code'] = 401;
+      $response['message'] = 'Please wait for the confirmation';
+      return response()->json($response);
     }
+
+    $data['token'] = auth()->claims([
+      'user_id' => $user->id,
+      'email' => $user->email
+    ])->attempt($credentials);
+
+    $response['data'] = $data;
+    $response['status'] = 1;
+    $response['code'] = 200;
+    $response['message'] = 'Login successful';
+
+    return response()->json($response);
+  }
+
+  public function sendVerificationEmail($student) {
+    $verificationCode = $student->email_auth_token;
+
+    Mail::to($student->email)->send(new VerificationMail($verificationCode));
+  }
+
+  public function retrieve(Request $request, $id, $email) {
+    try {
+      // Verify JWT token
+      $user = JWTAuth::parseToken()->authenticate();
+    } catch (TokenExpiredException $e) {
+      return response()->json(['message' => 'Token expired'], 401);
+    } catch (TokenInvalidException $e) {
+      return response()->json(['message' => 'Token invalid'], 401);
+    } catch (JWTException $e) {
+      return response()->json(['message' => 'Token absent'], 401);
+    }
+
+    // Check if the user's ID and email match the parameters
+    if ($user->id == $id && $user->email == $email) {
+      // Retrieve student and user information based on user_id
+      $student = Students::where('user_id', $id)->first();
+      $userInfo = User::where('email', $email)->first();
+
+      if ($student && $userInfo) {
+        return response()->json([
+          'first_name' => $student->first_name,
+          'last_name' => $student->last_name,
+          'email' => $userInfo->email,
+          'student_number' => $student->student_number,
+          'birthday' => $student->birthday,
+          'gender' => $student->gender,
+          'user_id' => $student->user_id,
+          'role_id' => $student->role_id,
+          'account_status_id' => $student->account_status_id,
+          'icon_id' => $student->icon_id
+        ]);
+      } else {
+        // Student or user not found
+        return response()->json(['message' => 'Student or user not found'], 404);
+      }
+    } else {
+      // Unauthorized access
+      return response()->json(['message' => 'Unauthorized'], 403);
+    }
+  }
+
+  public function updateIcon(Request $request) {
+    try {
+      // Verify JWT token
+      $user = JWTAuth::parseToken()->authenticate();
+    } catch (TokenExpiredException $e) {
+      return response()->json(['message' => 'Token expired'], 401);
+    } catch (TokenInvalidException $e) {
+      return response()->json(['message' => 'Token invalid'], 401);
+    } catch (JWTException $e) {
+      return response()->json(['message' => 'Token absent'], 401);
+    }
+
+
+    $user_id = $user->id;
+
+    $request->validate([
+      'icon_id' => 'required|integer|'
+    ]);
+
+    $updated = Students::where('user_id', $user_id)->update(['icon_id' => $request->icon_id]);
+
+    if ($updated) {
+      return response()->json(['message' => 'Icon ID updated successfully'], 200);
+    } else {
+      return response()->json(['message' => 'Failed to update icon ID'], 500);
+    }
+  }
 }
