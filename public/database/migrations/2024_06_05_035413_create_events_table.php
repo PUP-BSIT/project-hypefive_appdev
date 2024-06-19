60d265e4a45bc62691299af987dd196b3bc75fb1
Villa-VillaDiana <diana.villavilla02@gmail.com>
2024-06-19 19:26:33 +0800

    fix bugs

---
+++ public/database/migrations/2024_06_05_035413_create_events_table.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('event_name', 50);
+            $table->string('location', 50);
+            $table->date('date');
+            $table->time('time');
+            //By default, all members are NOT REQUIRED
+            $table->boolean('all_members_required')->default(false); 
+            //By default, the event DOESN"T HAVE A FEE
+            $table->boolean('has_reg_fee')->default(false); 
+            $table->integer('registration_fee')->nullable();
+            $table->integer('max_attendees');
+            $table->string('caption', 850);
+            $table->string('poster_loc', 255)->nullable(); //temporary
+            $table->unsignedBigInteger('event_status_id')->default(1);
+            $table->unsignedBigInteger('event_state_id')->default(1);
+            $table->timestamps();
+
+            $table->foreign('event_status_id')->references('id')
+                ->on('event_status')->onDelete('CASCADE')->onUpdate('CASCADE');
+            $table->foreign('event_state_id')->references('id')
+                ->on('event_state')->onDelete('CASCADE')->onUpdate('CASCADE');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
---
+++ public/routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\CheckToken;
+use App\Http\Controllers\EmailVerificationController;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Check token
+Route::middleware([CheckToken::class])->group(function () {
+    Route::get('/login', function () {
+        return view('welcome');
+    });
+
+    Route::get('/freedom-wall', function () {
+        return view('welcome');
+    });
+
+    Route::get('/events', function () {
+        return view('welcome');
+    });
+
+    Route::get('/members', function () {
+        return view('welcome');
+    });
+
+    Route::get('/archive', function () {
+        return view('welcome');
+    });
+});
+
+Route::get('/test-email', function () {
+    $user = \App\Models\User::first();
+    $controller = new \App\Http\Controllers\StudentsController();
+    $controller->sendVerificationEmail($user);
+    return 'Email sent';
+});
