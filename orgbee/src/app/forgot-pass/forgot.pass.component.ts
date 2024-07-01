4dbd7db1a937b392439f83a32d0422be500e3bdf
Yves John B. Tapispisan <bancolitayves@gmail.com>
2024-07-01 16:12:56 +0800

    add service to forgot pass

--- orgbee/src/app/forgot-pass/forgot.pass.component.ts
+++ orgbee/src/app/forgot-pass/forgot.pass.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
-
-interface ApiResponse {
-  success: boolean;
-  message: string;
-}
+import { ForgotPassService } from '../../service/forgotpass.service'
 
 @Component({
   selector: 'app-forgot-password',
@@ -24,7 +20,8 @@ export class ForgotPassComponent {
   constructor(
     private fb: FormBuilder,
     private http: HttpClient,
-    private router: Router
+    private router: Router,
+    private forgotPassService: ForgotPassService
   ) {
     this.emailForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
@@ -53,91 +50,77 @@ export class ForgotPassComponent {
   sendVerificationCode() {
     if (this.emailForm.valid) {
       this.loading = true;
-      const url = `http://localhost:8000/api/auth/send-reset-link`;
-      this.http
-        .post<ApiResponse>(url, { email: this.emailForm.value.email })
-        .subscribe({
-          next: (response) => {
-            this.loading = false;
-            if (response.success) {
-              this.step = 2;
-              this.errorMessage = '';
-            } else {
-              this.errorMessage = response.message;
-            }
-          },
-          error: (error) => {
-            this.loading = false;
-            if (error.status === 404 && error.error.message === 'Email not found') {
-              this.errorMessage = 'Email not found';
-            } else {
-              this.errorMessage = 'An error occurred. Please try again.';
-            }
-          },
-        });
+      this.forgotPassService.sendResetLink(this.emailForm.value.email).subscribe({
+        next: (response) => {
+          this.loading = false;
+          if (response.success) {
+            this.step = 2;
+            this.errorMessage = '';
+          } else {
+            this.errorMessage = response.message;
+          }
+        },
+        error: (error) => {
+          this.loading = false;
+          if (error.status === 404 && error.error.message === 'Email not found') {
+            this.errorMessage = 'Email not found';
+          } else {
+            this.errorMessage = 'An error occurred. Please try again.';
+          }
+        },
+      });
     }
   }
 
   verifyCode() {
     if (this.codeForm.valid) {
       this.loading = true;
-      const url = `http://localhost:8000/api/auth/verify-code`;
-      this.http
-        .post<ApiResponse>(url, {
-          email: this.emailForm.value.email,
-          token: this.codeForm.value.code
-        })
+      this.forgotPassService.verifyCode(this.emailForm.value.email, 
+            this.codeForm.value.code)
         .subscribe({
-          next: (response) => {
-            if (response.success) {
-              this.loading = false;
-              this.step = 3; 
-              this.errorMessage = '';
-            } else {
-              this.loading = false;
-              this.errorMessage = response.message;
-            }
-          },
-          error: (error) => {
-            this.loading = false;
-            if (error.status === 401 && error.error.message === 'Incorrect token') {
-              this.errorMessage = 'Incorrect token';
-            } else {
-              this.errorMessage = 'An error occurred. Please try again.';
-            }
-          },
-        });
-    }
-  }
-
-  updatePassword() {
-    if (this.passwordForm.valid && !this.passwordForm.hasError('mismatch')) {
-      this.loading = true;
-      const url = `http://localhost:8000/api/auth/reset-password`;
-      this.http
-        .post<ApiResponse>(url, {
-          email: this.emailForm.value.email,
-          password: this.passwordForm.value.newPassword,
-          password_confirmation: this.passwordForm.value.confirmPassword
-        })
-        .subscribe({
-          next: (response) => {
-            if (response.success) {
-              this.loading = false;
-              this.step = 4; 
-            } else {
-              this.loading = false;
-              this.errorMessage = response.message;
-            }
-          },
-          error: (error) => {
-            this.loading = false;
-            this.errorMessage = error.message;
-          },
-        });
+        next: (response) => {
+          this.loading = false;
+          if (response.success) {
+            this.step = 3; 
+            this.errorMessage = '';
+          } else {
+            this.errorMessage = response.message;
+          }
+        },
+        error: (error) => {
+          this.loading = false;
+          if (error.status === 401 && error.error.message === 'Incorrect token') {
+            this.errorMessage = 'Incorrect token';
+          } else {
+            this.errorMessage = 'An error occurred. Please try again.';
+          }
+        },
+      });
     }
   }
   
+  updatePassword() {
+    if (this.passwordForm.valid && !this.passwordForm.hasError('mismatch')) {
+      this.loading = true;
+      this.forgotPassService.resetPassword(this.emailForm.value.email, 
+            this.passwordForm.value.newPassword, 
+            this.passwordForm.value.confirmPassword)
+        .subscribe({
+        next: (response) => {
+          this.loading = false;
+          if (response.success) {
+            this.step = 4; 
+          } else {
+            this.errorMessage = response.message;
+          }
+        },
+        error: (error) => {
+          this.loading = false;
+          this.errorMessage = error.message;
+        },
+      });
+    }
+  }
 
   redirectToLogin() {
     this.router.navigate(['/login']);
