83760fd1c99e21361e0a70bfd4bdf77ad1f919bd
Villa-VillaDiana <diana.villavilla02@gmail.com>
2024-07-06 07:21:09 +0800

    update event poster location

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,5 +14,5 @@
 </style><link rel="stylesheet" href="styles-YZC72MRI.css" media="print" onload="this.media='all'"><noscript><link rel="stylesheet" href="styles-YZC72MRI.css"></noscript><link rel="modulepreload" href="chunk-NUCJ6FX4.js"></head>
 <body>
   <app-root></app-root>
-<script src="polyfills-6EAL64PA.js" type="module"></script><script src="main-AC7O2EET.js" type="module"></script></body>
+<script src="polyfills-6EAL64PA.js" type="module"></script><script src="main-W2LCICO6.js" type="module"></script></body>
 </html>
