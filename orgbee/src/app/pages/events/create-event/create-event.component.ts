48b60d3ec6482577f2073f344c5e57a4b0947615
cainday_bradley <bradleycainday@gmail.com>
2024-07-07 12:13:27 +0800

    rename into event fee and disable the past dates

--- orgbee/src/app/pages/events/create-event/create-event.component.ts
+++ orgbee/src/app/pages/events/create-event/create-event.component.ts
@@ -22,6 +22,7 @@ export class CreateEventComponent implements OnInit {
   preview: string;
   response: Response;
   file: File;
+  minDate: string;  
 
   constructor (
     private formBuilder: FormBuilder, 
@@ -30,6 +31,8 @@ export class CreateEventComponent implements OnInit {
   ) {}
 
   ngOnInit(): void {
+    this.minDate = new Date().toISOString().split('T')[0];  
+
     this.eventForm = this.formBuilder.group({
       event_name: 
         ['', {validators: [Validators.required, Validators.maxLength(100)],}],
